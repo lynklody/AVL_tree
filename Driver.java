5d66d554fb57c9d4c236ab5e5e897f8c0604e015
Wendy Liu <ailiuwendy@gmail.com>
2018-07-24 16:58:43 -0700

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -3,6 +3,7 @@ public class Driver
 	public static void main(String [] args) 
 	{ 
 		Tree t = new Tree();
+		// CHANGE INPUT HERE!
 		int [] input = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 }; 
 		for (int i = 0; i < input.length; ++i) 
 		{
@@ -11,6 +12,5 @@ public class Driver
 		System.out.printf("Here is tree (verbose):\n%s\n", t.toStringVerbose()); 
 		System.out.printf("A little prettier format:\n%s\n", t.toString());
 		System.out.printf("The height is %d\n", t.findTreeHeight()); 
-		// We're not testing removals. } } 
 	}
 }
